7b5815df5a814401921773933cfb14d02d3b7100
Hoàng Phúc Nguyễn Văn <phucknguyen123456789@gmail.com>
2025-05-26 20:01:28 +0700

    feat: create url + permission

--- app/serializers.py
+++ app/serializers.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 
 class UploadImageSerializer(serializers.Serializer):
     image = serializers.ImageField()
+
+
+class ResponseSerializer(serializers.Serializer):
+    message = serializers.CharField(max_length=255)
+    status = serializers.BooleanField(default=True)
--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     path('auth/', include('authentication.urls')),
     path('upload/', views.UploadImageView.as_view(), name='upload-image'),
     path('', include('users.urls')),
+    path('', include('coupons.urls')),
+    path('', include('products.urls')),
+    path('', include('carts.urls')),
+    path('', include('orders.urls')),
+    path('', include('payments.urls')),
+    path('', include('reviews.urls')),
+    path('', include('shipping.urls')),
+    path('', include('wishlist.urls')),
 ]
