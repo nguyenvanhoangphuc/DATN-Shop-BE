def9e04c0c2e820fce271cf9608066885c77c28b
Hoàng Phúc Nguyễn Văn <phucknguyen123456789@gmail.com>
2025-05-26 20:04:13 +0700

    feat: full api order

--- orders/models.py
+++ orders/models.py
@@ -45,19 +45,3 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} x {self.sub_product} in Order {self.order.id}"
-
-
-# ==========================
-# CART ITEM MODEL
-# ==========================
-class CartItem(models.Model):
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cart_items")
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="cart_items")
-    # sub_product = models.ForeignKey("SubProduct", on_delete=models.CASCADE, related_name="cart_items")
-    sub_product = models.ForeignKey('products.SubProduct', on_delete=models.CASCADE, related_name="cart_items")
-    quantity = models.IntegerField(default=1)
-    created_at = models.DateTimeField(auto_now_add=True)
-    status_enum = models.IntegerField(choices=StatusEnum.choices, default=StatusEnum.ACTIVE)
-
-    def __str__(self):
-        return f"{self.quantity} x {self.sub_product} in {self.user.username}'s cart"
