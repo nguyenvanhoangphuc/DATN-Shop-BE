30147081482f33378fe4a3dc35174b172fe3e5eb
Hoàng Phúc Nguyễn Văn <phucknguyen123456789@gmail.com>
2025-06-10 22:25:03 +0700

    feat: api chatbot

---
+++ chat_app/serializers.py
@@ -0,0 +1,8 @@
+# # chat_app/serializers.py
+# from rest_framework import serializers
+
+# class ChatRequestSerializer(serializers.Serializer):
+#     query = serializers.CharField()
+
+# class ChatResponseSerializer(serializers.Serializer):
+#     response = serializers.CharField()
