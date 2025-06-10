1b3bcb19cb35c7d10cc726341bd1323d6f8b76d3
Hoàng Phúc Nguyễn Văn <phucknguyen123456789@gmail.com>
2025-06-10 22:26:10 +0700

    feat: update api users

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
-router = DefaultRouter()
+router = SimpleRouter(trailing_slash=False)
 router.register(r'user', views.UserViewSet, basename='user')
 
 urlpatterns = [
-    path('user/search-and/', views.UserSearchView.as_view(), name='user-search-and'),
-    path('user/search-or/', views.UserSearchViewOR.as_view(), name='user-search-or'),
+    path('user/search-and', views.UserSearchView.as_view(), name='user-search-and'),
+    path('user/search-or', views.UserSearchViewOR.as_view(), name='user-search-or'),
     path('', include(router.urls)),
 ]
 
